b90f7fe8e557d63c4a2adac09d2ee7fe90ea4e9b
bddaax <brenda14705@gmail.com>
2025-05-16 11:54:19 +0700

    [BONUS] Run experiment on cloud.

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
 
 fn main() {
     let mut p = CrosstownBus::new_queue_publisher("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
+    // bonus : let mut p = CrosstownBus::new_queue_publisher("amqps://qwnwqife:YKHKIRF2M6uyXjbCprsw4LAyol7VaWDb@armadillo.rmq.cloudamqp.com/qwnwqife".to_owned()).unwrap();
     _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
     user_id: "1".to_owned(), user_name: "2306152304-Amir".to_owned() });
     _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
